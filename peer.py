bae0c2ceba2467c0f9e805d43ce617c2487e41b6
jasenovski <igormjsk@gmail.com>
2026-07-06 21:23:05 -0300

    chore: add test print statement for debugging purposes

--- peer.py
+++ peer.py
@@ -74,3 +74,5 @@ print(f"Tempo de mineração: {mining_time.total_seconds() / 60:.2f} minutos")
 response = requests.put(f"{PEER}/check_mining", json=bloco)
 
 print(response.json())
+
+print("teste")
